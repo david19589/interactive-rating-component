905c7ba36de35f93aacd16eaffc63f21a6013671
David Naskidashvili <132206350+david19589@users.noreply.github.com>
2024-01-23 14:30:29 +0400

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import "./Header.css";
+import IconStar from "/assets/icon-star.svg"
 
 const Header = () => {
     return (
         <div>
-            <img className="star" src="src/assets/icon-star.svg" alt="star" />
+            <img className="star" src={IconStar} alt="star" />
             <h1 className="h1">How did we do?</h1>
             <p className="p">Please let us know how we did with
                 your support request. All feedback is appreciated
